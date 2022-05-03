937d00433eda9d4e2d1d89b6c40993f419f544f0
Thelofx123 <gaanbuud106@gmail.com>
2022-05-03 11:12:00 +0800

    hello

--- todo.js
+++ todo.js
@@ -10,11 +10,23 @@ function add(){
     }
     document.getElementById("myInput").value  = "";
     var input = document.createElement('SPAN');
+    var input2 = document.createElement('SPAN');
+    var edit = document.createElement('SPAN');
+    var eText = document.createTextNode("\u270E");
     var testt = document.createTextNode("\u00D7");
+    var checkb = document.createTextNode("\u2713");
+
 
     input.className = "close";
+    input2.className = "che1";
+    edit.className = "edit";
+    edit.appendChild(eText)
     input.appendChild(testt)
+    input2.appendChild(checkb)
     li.appendChild(input)
+    li.appendChild(input2)
+    li.appendChild(edit)
+
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
@@ -22,6 +34,21 @@ function add(){
           div.style.display = "none";
         }
       }
+
+      for (j = 0; j < che.length; j++) {
+        che[j].onclick = function() {
+          var div = this.parentElement;
+          div.classList.toggle('toggle');
+        }
+      }
+
+      for (var e = 0; e < editC.length; e++) {
+       
+        editC[e].onclick = function () {
+          var p = prompt("Edit your entry");
+          entry.innerHTML = p;
+         }
+        }
 }
 
 document.addEventListener("keypress", function (event) {
@@ -30,11 +57,16 @@ document.addEventListener("keypress", function (event) {
   }
 });
 
+
 var close = document.getElementsByClassName("close");
 let i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+
+
+
+var che = document.getElementsByClassName("che1");
+let j;
+
+
+var editC = document.getElementsByClassName("edit");
+let e;
+
